65bbcccf588347cfe5757cb47409b6e24b57a28c
AS75011 <sauvegrain.antony@gmail.com>
2018-09-27 23:24:19 +0200

    new docs

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import './containers/App.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
